b08d692721e7545f7d1a454e759a7940f1a99e3a
mauriciovaldora <mauricio_valdora@hotail.com.ar>
2023-08-01 17:27:49 -0300

    cambie cositas

--- src/components/search/Searcher.jsx
+++ src/components/search/Searcher.jsx
@@ -5,7 +5,6 @@ function Buscador({onChange}) {
     return (
         <div className='contenedor-buscador'>
             <input type="text" placeholder='Buscar' className='buscador' onChange={onChange} />
-            <button className='btn'><span class="material-symbols-outlined">search</span></button>
         </div>
     )
 
